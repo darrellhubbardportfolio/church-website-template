63146386e6c19c4aa9376ed41bccfcf817079446
Darrell Hubbard <thehubbard89@gmail.com>
2026-05-16 09:42:38 -0400

    update

--- routes/LoginRouter.js
+++ routes/LoginRouter.js
@@ -3,7 +3,7 @@ const { appendFile } = require("fs");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("login page");
+    res.render("login-page");
 });
 
 router.post("/verify", (req, res) => {
